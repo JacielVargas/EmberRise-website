1b58ff75aac02e5281e91de063b4fe1f23fa5fe1
JacielVargas <jacielvargas@yahoo.com>
2026-07-18 18:01:54 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// EmberRise JavaScript
+const siteHeader = document.querySelector(".site-header");
+
+function updateHeader() {
+    if (window.scrollY > 40) {
+        siteHeader.classList.add("scrolled");
+    } else {
+        siteHeader.classList.remove("scrolled");
+    }
+}
+
+window.addEventListener("scroll", updateHeader);
+
+updateHeader();
